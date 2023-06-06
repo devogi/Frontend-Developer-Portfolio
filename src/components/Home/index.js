d6d26b3e895f8d41291d194304c397cf36d7ea9a
Oguzhan <75084633+devogi@users.noreply.github.com>
2023-06-06 11:35:41 +0200

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -57,7 +57,7 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2>Front End Developer / JavaScript Keen / Based in Istanbul</h2>
+          <h2>Front End Developer / JavaScript Keen / Based in München</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
